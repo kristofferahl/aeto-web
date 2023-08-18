bac4bbf19f5e601aae8a5e7aeda9d6de0d6ed350
Kristoffer Ahl <mail@77dynamite.com>
2023-08-18 13:54:52 +0200

    Adding labels to resource filter.

--- server/filter.go
+++ server/filter.go
@@ -7,9 +7,9 @@ import (
 )
 
 func ApplyResourceListFilter(data []byte) ([]byte, error) {
-	return jsonfilter.Filter(data, "items(apiVersion,kind,metadata(annotations,creationTimestamp,finalizers,generation,name,namespace,resourceVersion,uid),spec,status)")
+	return jsonfilter.Filter(data, "items(apiVersion,kind,metadata(annotations,labels,creationTimestamp,finalizers,generation,name,namespace,resourceVersion,uid),spec,status)")
 }
 
 func ApplyResourceFilter(format string, data []byte) ([]byte, error) {
-	return jsonfilter.Filter(data, fmt.Sprintf(format, "apiVersion,kind,metadata(annotations,creationTimestamp,finalizers,generation,name,namespace,resourceVersion,uid),spec,status"))
+	return jsonfilter.Filter(data, fmt.Sprintf(format, "apiVersion,kind,metadata(annotations,labels,creationTimestamp,finalizers,generation,name,namespace,resourceVersion,uid),spec,status"))
 }
